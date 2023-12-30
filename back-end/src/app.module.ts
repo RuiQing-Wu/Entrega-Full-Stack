9d8a37b82b7874537ec0dc90ea613f4296b804e3
isaac1227 <i.marroquipenalva@um.es>
2023-12-30 20:23:59 +0100

    commit sin querer

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ComunidadModel } from './comunidades/entities/comunidades.model';
 import { ComunidadModule } from './comunidades/comunidades.module';
 //import { AuthModule } from './auth/auth.module';
 //import { UsersModule } from './users/users.module';
