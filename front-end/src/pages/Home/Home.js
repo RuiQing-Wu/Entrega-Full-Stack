ad30d6358c7fd44bbf0287b681d1b501f1c4c2e9
isaac1227 <i.marroquipenalva@um.es>
2024-01-04 18:02:32 +0100

    new home

--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -1,58 +1,46 @@
-// Importa useState para manejar el estado local del componente
-import React, { useState } from 'react';
-import CardComunidad from '../../component/CardComunidad';
-import NuevoContacto from '../../component/NuevoContacto';
+import React from 'react';
+import { Row, Col, Button, Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
 export default function Home({ comunidades = [] }) {
-  // Estado local para almacenar las comunidades
-  const [comunidadesLocales, setComunidadesLocales] = useState(comunidades);
+  const navigate = useNavigate();
 
-  // Función para añadir una comunidad ficticia
-  const agregarComunidadFicticia = () => {
-    const nuevaComunidad = {
-      id: 999, // Puedes ajustar el ID según tus necesidades
-      imageUrl: 'url_de_la_imagen',
-      title: 'Comunidad Ficticia',
-    };
-
-    // Actualiza el estado con la nueva comunidad ficticia
-    setComunidadesLocales([...comunidadesLocales, nuevaComunidad]);
-  };
-
-  function likeCausa(id) {
-    // eslint-disable-next-line no-console
-    console.log(id);
+  function handleRedireccionAComunidades() {
+    navigate('/listaComunidades');
   }
 
   return (
-    <div className="container mt-4">
-      <div className="row">
-        {/* Muestra las comunidades existentes */}
-        {comunidadesLocales.map((comunidad) => (
-          <div key={comunidad.id}>
-            <CardComunidad
-              imageUrl={comunidad.imageUrl}
-              title={comunidad.title}
-              onAddToCartClicked={() => likeCausa(comunidad.id)}
-            />
-          </div>
-        ))}
-
-        {/* Botón para añadir una comunidad ficticia */}
-        <div>
-          <button
-            className="btn btn-success"
-            onClick={agregarComunidadFicticia}
+    <div className="d-flex flex-column justify-content-center">
+      <Row className="bg-info-subtle">
+        <Col className="p-5">
+          <h1>SolidarianID</h1>
+          <h6>Conectando con los más necesitados</h6>
+        </Col>
+      </Row>
+      <Row className="bg-dark-subtle p-5">
+        <Col>
+          <h4>Quienes somos</h4>
+          <p>
+            SolidarianID es una plataforma destinada a fomentar la participación
+            de ciudadanos de todos los países en acciones solidarias. Nuestros
+            usuarios podrán organizarse en comunidades para fomentar la
+            interacción y colaboración entre ellos.
+          </p>
+        </Col>
+      </Row>
+      <Row className="bg-light p-5">
+        <Col>
+          <h5>Navega entre las comunidades de nuestra web</h5>
+          <Button
+            onClick={() => handleRedireccionAComunidades()}
+            variant="outline-info"
+            size="sm"
           >
-            Añadir Comunidad Ficticia
-          </button>
-        </div>
-
-        <div>
-          <NuevoContacto />
-        </div>
-      </div>
+            Ver comunidades
+          </Button>
+        </Col>
+      </Row>
     </div>
   );
 }
