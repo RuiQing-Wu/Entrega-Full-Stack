75fef1969cb3543c6965ededef537cca7da83c2c
isaac1227 <i.marroquipenalva@um.es>
2024-03-15 17:49:58 +0100

    validacion del formulario de publicaciones

--- blog/public/javascripts/validarFormulario.js
+++ blog/public/javascripts/validarFormulario.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const formulario = document.getElementById("formulario");
+  const publicacionInput = document.getElementById("publicaciones");
+  const comunidadSelect = document.getElementById("comunidad");
+  const errorDivComunidad = document.getElementById("error-comunidad");
+  const errorDivPublicacion = document.getElementById("error-publicacion");
+  const errorMessagePublicacion = document.getElementById(
+    "error-message-publicacion"
+  );
+  const errorMessageComunidad = document.getElementById(
+    "error-message-comunidad"
+  );
 
   formulario.addEventListener("submit", function (event) {
-    if (!validarFormulario()) {
-      event.preventDefault(); // Evitar que se envíe el formulario si no pasa la validación
-    }
-  });
+    event.preventDefault(); // Evita el envío automático del formulario
 
-  function validarFormulario() {
-    const publicacionInput = document.getElementById("publicaciones");
-    const comunidadSelect = document.getElementById("comunidad");
-
-    // Reiniciar mensajes de error
     ocultarError("comunidad");
     ocultarError("publicacion");
 
@@ -27,45 +29,24 @@ document.addEventListener("DOMContentLoaded", function () {
       valido = false;
     }
 
-    return valido;
-  }
+    if (valido) {
+      formulario.submit();
+    }
+  });
 
   function mostrarError(message, tipo) {
-    const errorDivComunidad = document.getElementById("error-comunidad");
+    const errorDiv =
+      tipo === "publicacion" ? errorDivPublicacion : errorDivComunidad;
+    const errorMessage =
+      tipo === "publicacion" ? errorMessagePublicacion : errorMessageComunidad;
 
-    if (tipo === "publicacion") {
-      const errorMessagePublicacion = document.getElementById(
-        "error-message-publicacion"
-      );
-      errorMessagePublicacion.textContent = message;
-      errorMessagePublicacion.style.display = "block"; // Mostrar mensaje de error
-    }
-
-    if (tipo === "comunidad") {
-      const errorMessageComunidad = document.getElementById(
-        "error-message-comunidad"
-      );
-      errorMessageComunidad.textContent = message;
-      errorDivComunidad.style.display = "block"; // Mostrar mensaje de error
-    }
+    errorMessage.textContent = message;
+    errorDiv.style.display = "block";
   }
 
   function ocultarError(tipo) {
-    const errorDivComunidad = document.getElementById("error-comunidad");
-    const errorMessagePublicacion = document.getElementById(
-      "error-message-publicacion"
-    );
-    const errorMessageComunidad = document.getElementById(
-      "error-message-comunidad"
-    );
-
-    if (tipo === "publicacion") {
-      errorMessagePublicacion.style.display = "none"; // Ocultar mensaje de error
-    }
-
-    if (tipo === "comunidad") {
-      errorMessageComunidad.style.display = "none"; // Ocultar mensaje de error
-      errorDivComunidad.style.display = "none"; // Ocultar contenedor de error
-    }
+    const errorDiv =
+      tipo === "publicacion" ? errorDivPublicacion : errorDivComunidad;
+    errorDiv.style.display = "none";
   }
 });
